a43e602a434ee645921dfe80e4005051671bd822
Jessica Berteloni <jessiberteloni@gmail.com>
2022-04-22 19:24:23 -0300

    Criado metodo de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -9,6 +9,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 if __name__ == '__main__':
     jessica = Pessoa(nome='Jessica')
     pedro = Pessoa(jessica, nome='Pedro')
@@ -30,3 +38,5 @@ if __name__ == '__main__':
     print(pedro.olhos)
     print(jessica.olhos)
     print(id(Pessoa.olhos), id(pedro.olhos), id(jessica.olhos))
+    print(Pessoa.metodo_estatico(), pedro.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), pedro.nome_e_atributos_de_classe())
